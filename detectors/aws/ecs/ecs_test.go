12859a8568ef1ffc894c91fad708cd8b41692ad8
Michele Mancioppi <michelem@lumigo.io>
2022-10-24 15:00:36 +0200

    Try this

--- detectors/aws/ecs/ecs_test.go
+++ detectors/aws/ecs/ecs_test.go
@@ -17,8 +17,8 @@ package ecs
 import (
 	"context"
 	"encoding/json"
-	// "net/http"
-	// "net/http/httptest"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	// "path/filepath"
 	// "strings"
@@ -183,6 +183,15 @@ func TestDetectV3(t *testing.T) {
 // 	assert.Equal(t, err, nil, "Detector should not fail")
 // 	assert.Equal(t, expectedResource, res, "Resource returned is incorrect")
 // }
+
+func TestGoOnWindowsSucks(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("SUKA")); err != nil {
+			t.Fatal(err)
+		}
+	}))
+	defer ts.Close()
+}
 
 // returns empty resource when detector cannot read container ID.
 func TestDetectCannotReadContainerID(t *testing.T) {
